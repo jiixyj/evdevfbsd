2795cb4ecfd983479dfceebfb0c1a2f9700f2a90
Jan Kokemüller <jan.kokemueller@gmail.com>
2014-12-03 01:44:59 +0100

    semi mt tracking

--- evdevfbsd.c
+++ evdevfbsd.c
@@ -302,6 +302,49 @@ int32_t synaptics_reverse_y(int32_t y) {
   return y;
 }
 
+int synaptics_setup_abs_axes(struct event_device *ed, struct psm_backend *b,
+                             int x_axis, int y_axis) {
+  set_bit(ed->abs_bits, x_axis);
+  set_bit(ed->abs_bits, y_axis);
+  ed->abs_info[x_axis].minimum = 1472;
+  ed->abs_info[x_axis].maximum = 5472;
+  ed->abs_info[y_axis].minimum = 1408;
+  ed->abs_info[y_axis].maximum = 4448;
+  switch (b->synaptics_info.infoSensor) {
+    case 1:
+      ed->abs_info[x_axis].resolution = 85;
+      ed->abs_info[y_axis].resolution = 94;
+      break;
+    case 2:
+      ed->abs_info[x_axis].resolution = 91;
+      ed->abs_info[y_axis].resolution = 124;
+      break;
+    case 3:
+      ed->abs_info[x_axis].resolution = 57;
+      ed->abs_info[y_axis].resolution = 58;
+      break;
+    case 8:
+      ed->abs_info[x_axis].resolution = 85;
+      ed->abs_info[y_axis].resolution = 94;
+      break;
+    case 9:
+      ed->abs_info[x_axis].resolution = 73;
+      ed->abs_info[y_axis].resolution = 96;
+      break;
+    case 11:
+      ed->abs_info[x_axis].resolution = 187;
+      ed->abs_info[y_axis].resolution = 170;
+      break;
+    case 12:
+      ed->abs_info[x_axis].resolution = 122;
+      ed->abs_info[y_axis].resolution = 167;
+      break;
+    default:
+      return 1;
+  }
+  return 0;
+}
+
 int psm_backend_init(struct event_device *ed) {
   ed->priv_ptr = malloc(sizeof(struct psm_backend));
   if (!ed->priv_ptr)
@@ -359,44 +402,21 @@ int psm_backend_init(struct event_device *ed) {
         set_bit(ed->key_bits, BTN_TOOL_TRIPLETAP);
       }
 
-      set_bit(ed->abs_bits, ABS_X);
-      set_bit(ed->abs_bits, ABS_Y);
-      ed->abs_info[ABS_X].minimum = 1472;
-      ed->abs_info[ABS_X].maximum = 5472;
-      ed->abs_info[ABS_Y].minimum = 1408;
-      ed->abs_info[ABS_Y].maximum = 4448;
-      switch (b->synaptics_info.infoSensor) {
-        case 1:
-          ed->abs_info[ABS_X].resolution = 85;
-          ed->abs_info[ABS_Y].resolution = 94;
-          break;
-        case 2:
-          ed->abs_info[ABS_X].resolution = 91;
-          ed->abs_info[ABS_Y].resolution = 124;
-          break;
-        case 3:
-          ed->abs_info[ABS_X].resolution = 57;
-          ed->abs_info[ABS_Y].resolution = 58;
-          break;
-        case 8:
-          ed->abs_info[ABS_X].resolution = 85;
-          ed->abs_info[ABS_Y].resolution = 94;
-          break;
-        case 9:
-          ed->abs_info[ABS_X].resolution = 73;
-          ed->abs_info[ABS_Y].resolution = 96;
-          break;
-        case 11:
-          ed->abs_info[ABS_X].resolution = 187;
-          ed->abs_info[ABS_Y].resolution = 170;
-          break;
-        case 12:
-          ed->abs_info[ABS_X].resolution = 122;
-          ed->abs_info[ABS_Y].resolution = 167;
-          break;
-        default:
+      if (synaptics_setup_abs_axes(ed, b, ABS_X, ABS_Y)) 
+        goto fail;
+
+      if (b->synaptics_info.capAdvancedGestures) {
+        if (synaptics_setup_abs_axes(ed, b, ABS_MT_POSITION_X,
+                                     ABS_MT_POSITION_Y))
           goto fail;
+        set_bit(ed->abs_bits, ABS_MT_SLOT);
+        ed->abs_info[ABS_MT_SLOT].minimum = 0;
+        ed->abs_info[ABS_MT_SLOT].maximum = 1;
+        set_bit(ed->abs_bits, ABS_MT_TRACKING_ID);
+        ed->abs_info[ABS_MT_TRACKING_ID].minimum = 0;
+        ed->abs_info[ABS_MT_TRACKING_ID].maximum = 0xffff;
       }
+
       set_bit(ed->abs_bits, ABS_PRESSURE);
       ed->abs_info[ABS_PRESSURE].minimum = 0;
       ed->abs_info[ABS_PRESSURE].maximum = 255;
@@ -474,6 +494,9 @@ void* psm_fill_function(struct event_device *ed) {
   }
 
   int obuttons = 0;
+  int oslot0 = 0;
+  int oslot1 = 0;
+  uint16_t tracking_ids = 0;
   unsigned char packet[PACKET_MAX];
 
   while (read_full_packet(b->fd, packet, packetsize) == 0) {
@@ -490,8 +513,8 @@ void* psm_fill_function(struct event_device *ed) {
           int w = ((packet[0] & 0x30) >> 2) | ((packet[0] & 0x04) >> 1) |
                   ((packet[3] & 0x04) >> 2);
 
-          printf("%3d %3d %3d %3d %3d %3d %d\n", packet[0], packet[1],
-                 packet[2], packet[3], packet[4], packet[5], w);
+          // printf("%3d %3d %3d %3d %3d %3d %d\n", packet[0], packet[1],
+          //        packet[2], packet[3], packet[4], packet[5], w);
 
           if (w == 3) {
             packet[0] = packet[1];
@@ -557,7 +580,44 @@ void* psm_fill_function(struct event_device *ed) {
             put_event(ed, &tv, EV_KEY, BTN_BACK, !!(buttons & (1 << 6)));
 
           if (b->synaptics_info.capAdvancedGestures) {
-            // TODO
+            if (no_fingers >= 2) {
+              put_event(ed, &tv, EV_ABS, ABS_MT_SLOT, 0);
+              if (!oslot0)
+                put_event(ed, &tv, EV_ABS, ABS_MT_TRACKING_ID, ++tracking_ids);
+              put_event(ed, &tv, EV_ABS, ABS_MT_POSITION_X, MIN(x, b->ews.x));
+              put_event(ed, &tv, EV_ABS, ABS_MT_POSITION_X,
+                        synaptics_reverse_y(MIN(y, b->ews.y)));
+              put_event(ed, &tv, EV_ABS, ABS_MT_SLOT, 1);
+              if (!oslot1)
+                put_event(ed, &tv, EV_ABS, ABS_MT_TRACKING_ID, ++tracking_ids);
+              put_event(ed, &tv, EV_ABS, ABS_MT_POSITION_X, MAX(x, b->ews.x));
+              put_event(ed, &tv, EV_ABS, ABS_MT_POSITION_X,
+                        synaptics_reverse_y(MAX(y, b->ews.y)));
+              oslot0 = oslot1 = 1;
+            } else if (no_fingers == 1) {
+              put_event(ed, &tv, EV_ABS, ABS_MT_SLOT, 0);
+              if (!oslot0)
+                put_event(ed, &tv, EV_ABS, ABS_MT_TRACKING_ID, ++tracking_ids);
+              put_event(ed, &tv, EV_ABS, ABS_MT_POSITION_X, x);
+              put_event(ed, &tv, EV_ABS, ABS_MT_POSITION_X,
+                        synaptics_reverse_y(y));
+              if (oslot1) {
+                put_event(ed, &tv, EV_ABS, ABS_MT_SLOT, 1);
+                put_event(ed, &tv, EV_ABS, ABS_MT_TRACKING_ID, -1);
+              }
+              oslot0 = 1;
+              oslot1 = 0;
+            } else {
+              if (oslot0) {
+                put_event(ed, &tv, EV_ABS, ABS_MT_SLOT, 0);
+                put_event(ed, &tv, EV_ABS, ABS_MT_TRACKING_ID, -1);
+              }
+              if (oslot1) {
+                put_event(ed, &tv, EV_ABS, ABS_MT_SLOT, 1);
+                put_event(ed, &tv, EV_ABS, ABS_MT_TRACKING_ID, -1);
+              }
+              oslot0 = oslot1 = 0;
+            }
           }
 
           if (z > 30)
