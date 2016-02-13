17cba1a8b92e130592048f6135f112d8e099badd
Jan Kokemüller <jan.kokemueller@gmail.com>
2016-02-13 09:44:39 +0100

    reformat backend-psm.h

--- backend-psm.h
+++ backend-psm.h
@@ -10,32 +10,32 @@
 #include "evdevfbsd.h"
 
 struct synaptics_ew_state {
-  int32_t x;
-  int32_t y;
-  int32_t z;
+	int32_t x;
+	int32_t y;
+	int32_t z;
 };
 
 struct synaptics_slot_state {
-  int32_t x;
-  int32_t y;
-  int32_t tracking_id;
+	int32_t x;
+	int32_t y;
+	int32_t tracking_id;
 };
 
 struct psm_backend {
-  int fd;
-  mousehw_t hw_info;
-  int guest_dev_fd;
+	int fd;
+	mousehw_t hw_info;
+	int guest_dev_fd;
 
-  // synaptics stuff
-  synapticshw_t synaptics_info;
-  struct synaptics_ew_state ews;
-  struct synaptics_slot_state ss[2];
+	// synaptics stuff
+	synapticshw_t synaptics_info;
+	struct synaptics_ew_state ews;
+	struct synaptics_slot_state ss[2];
 };
 
 int psm_backend_init(struct event_device *ed);
 void *psm_fill_function(struct event_device *ed);
 
-int event_device_open_as_guest(struct event_device *ed,
-                               struct event_device *parent);
+int event_device_open_as_guest(
+    struct event_device *ed, struct event_device *parent);
 
 #endif
