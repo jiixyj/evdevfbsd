a0113629036dec843a6d206627406195fdc545a3
Jan Kokemüller <jan.kokemueller@gmail.com>
2016-02-13 09:49:19 +0100

    reformat backend-sysmouse.h

--- backend-sysmouse.h
+++ backend-sysmouse.h
@@ -6,11 +6,11 @@
 #include "evdevfbsd.h"
 
 struct sysmouse_backend {
-  int fd;
-  int level;
-  mousemode_t mode;
-  mousehw_t hw_info;
-  char path[32];
+	int fd;
+	int level;
+	mousemode_t mode;
+	mousehw_t hw_info;
+	char path[32];
 };
 
 int sysmouse_backend_init(struct event_device *ed, char const *path);
