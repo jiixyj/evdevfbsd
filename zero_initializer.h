ea89727caf49be43f6451730eb4b72297937698a
Jan Kokemüller <jan.kokemueller@gmail.com>
2016-02-13 10:22:16 +0100

    formatting

--- zero_initializer.h
+++ zero_initializer.h
@@ -1,16 +1,22 @@
 #ifndef ZERO_INITIALIZER_H_
 #define ZERO_INITIALIZER_H_
 
+// clang-format off
+
 #ifdef __cplusplus
-#define ZERO_INITIALIZER {}
+#define ZERO_INITIALIZER                                                      \
+	{ }
 #elif defined(__clang__)
-#define ZERO_INITIALIZER \
-_Pragma("clang diagnostic push") \
-_Pragma("clang diagnostic ignored \"-Wmissing-field-initializers\"") \
-{0} \
-_Pragma("clang diagnostic pop")
+#define ZERO_INITIALIZER                                                      \
+	_Pragma("clang diagnostic push")                                      \
+	_Pragma("clang diagnostic ignored \"-Wmissing-field-initializers\"")  \
+	{ 0 }                                                                 \
+	_Pragma("clang diagnostic pop")
 #else
-#define ZERO_INITIALIZER {0}
+#define ZERO_INITIALIZER                                                      \
+	{ 0 }
 #endif
+
+// clang-format on
 
 #endif // ZERO_INITIALIZER_H_
